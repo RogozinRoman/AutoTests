6e23bd430614850d3a317350d8c08fc3a04b3fb7
rogozin_roman <rv.rogozin@gmail.com>
2018-09-23 21:44:03 +0300

    add api tests

--- src/test/java/api/DiskYandexTest.java
+++ src/test/java/api/DiskYandexTest.java
@@ -1,14 +1,13 @@
 package api;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Title;
 
-import java.io.IOException;
-
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.IsNot.not;
 
 /**
@@ -20,25 +19,60 @@ public class DiskYandexTest {
     @Title("Отображение списка всех файлов")
     @Test
     public void showMeFiles() {
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url("https://cloud-api.yandex.net/v1/disk/resources/files")
-                .get()
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "AQAAAAAQSJGtAADLW72eHsFpPU0nqDWYuBurU7Q")
-                .addHeader("Cache-Control", "no-cache")
-                .build();
-
-        Response response;
-        String responseText = "";
         try {
-            response = client.newCall(request).execute();
-            responseText = response.body().string();
-            System.out.println("Body: " + responseText);
-        } catch (IOException e) {
+            HttpResponse<String> response = Unirest.get("https://cloud-api.yandex.net/v1/disk/resources/files")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "AQAAAAAQSJGtAADLW72eHsFpPU0nqDWYuBurU7Q")
+                    .header("Cache-Control", "no-cache")
+                    .asString();
+            System.out.println("Body: " + response.getBody());
+            Assert.assertThat(response.getBody(), not(""));
+        } catch (UnirestException e) {
             e.printStackTrace();
         }
-        Assert.assertThat(responseText, not(""));
     }
+
+    @Title("Проверка существования файла Зима.jpg")
+    @Test
+    public void fileExists() {
+        try {
+            HttpResponse<String> response = Unirest.get("https://cloud-api.yandex.net/v1/disk/resources/files")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "AQAAAAAQSJGtAADLW72eHsFpPU0nqDWYuBurU7Q")
+                    .header("Cache-Control", "no-cache")
+                    .asString();
+            System.out.println("Body: " + response.getBody());
+            Assert.assertThat(response.getBody(), containsString("Зима.jpg"));
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Title("Создать и удалить папку Test")
+    @Test
+    public void createAndDeleteFolder(){
+        try {
+            // Create
+            HttpResponse<String> responseCreate = Unirest.put("https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2FTest")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "AQAAAAAQSJGtAADLW72eHsFpPU0nqDWYuBurU7Q")
+                    .header("Cache-Control", "no-cache")
+                    .asString();
+            System.out.println("Body: " + responseCreate.getBody());
+            Assert.assertThat(responseCreate.getBody(), containsString("https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2FTest"));
+
+            // Delete
+            HttpResponse<String> responseDelete = Unirest.delete("https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2FTest")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "AQAAAAAQSJGtAADLW72eHsFpPU0nqDWYuBurU7Q")
+                    .header("Cache-Control", "no-cache")
+                    .asString();
+            System.out.println("Body: " + responseDelete.getBody());
+
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
